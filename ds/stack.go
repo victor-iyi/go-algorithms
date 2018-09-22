1b6f544fe2d691203225f33129383b43e73b921f
Victor I. Afolabi <javafolabi@gmail.com>
2018-09-22 08:49:47 +0100

    StackData, Peek, Pop, Push

--- ds/stack.go
+++ ds/stack.go
@@ -1,24 +1,46 @@
 package ds
 
+import "errors"
+
+// StackData contains data in a stack.
+type StackData float64
+
 // Stack is a data structure that abides to the Last In Last Out (LIFO) lingo.
 type Stack struct {
-	// An internal element count maintained by stack.
-	count uint
+	data  []StackData // Contains stak data.
+	count uint        // An internal element count maintained by stack.
+}
+
+// NewStack creates a new instance of a stack.
+func NewStack() *Stack {
+	return &Stack{data: nil, count: 0}
 }
 
 // Peek returns the last element in the stack.
-func (s *Stack) Peek() {
+func (s *Stack) Peek() (StackData, error) {
+	if s.count == 0 {
+		return 0, errors.New("StackError: Stack is empty")
+	}
 
+	return s.data[s.count], nil
 }
 
 // Pop removes the last element from the stack.
 func (s *Stack) Pop() {
+	if s.count == 0 {
 
+	}
+	// Decrement count.
+	s.count--
 }
 
 // Push adds a new element to the end of a stack.
-func (s *Stack) Push() {
+func (s *Stack) Push(data StackData) {
+	// Adds new data into the stack.
+	s.data = append(s.data, data)
 
+	// Increment count after successful push.
+	s.count = uint(len(s.data))
 }
 
 // Count returns the number of elements in a stack.
